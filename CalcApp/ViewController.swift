4fea392d0765b0d95f6697733a8438294ef3c004
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-27 21:22:15 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by ゆり on 2021/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
