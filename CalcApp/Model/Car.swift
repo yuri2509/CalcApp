fc65f8eab20973cce0fbf9a0e7eb70b03acfebd6
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-30 18:08:11 +0900

    fileとclassの追加

---
+++ CalcApp/Model/Car.swift
@@ -0,0 +1,21 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by ゆり on 2021/03/30.
+//
+
+import Foundation
+
+class Car {
+    
+    var frontWheel = 0
+    var rearWheel = 0
+    
+    //イニシャライズの略(初期化のこと）　必ず書くこと
+    init() {
+        
+    }
+    
+    
+}
